5a3b2a141eefc75c21e7e52055224ef29f1eb055
sina <sinaolfati5@gmail.com>
2022-06-09 17:42:56 +0430

    first typescript code

---
+++ sina.js
@@ -0,0 +1,5 @@
+var a = "sina";
+var b = 10;
+var c = true;
+var d = "sina";
+console.log(a);
---
+++ sina.ts
@@ -0,0 +1,5 @@
+let a : string = "sina";
+let b : number = 10;
+let c : boolean = true;
+let d : any = "sina";
+console.log(a)
