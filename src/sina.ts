a0d9cf91a25b0120e3e2d0efbe8aa188a19fa382
sina <sinaolfati5@gmail.com>
2022-06-12 15:41:22 +0430

    seeing the "implicitly" error of any types

--- src/sina.ts
+++ src/sina.ts
@@ -3,7 +3,13 @@
 // let c : boolean = true;
 // let d : any = "sina";
 // console.log(a)
-let age: number = 20
-if (age < 50)
-    age += 10
-console.log(age)
+
+
+// let age: number = 20
+// if (age < 50)
+//     age += 10
+// console.log(age)
+
+function render(document) {
+    console.log(document)
+}
